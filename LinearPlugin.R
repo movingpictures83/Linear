b8b3568b27ae9edd8b4879e1eb63aa3e3d51b055
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-11-25 20:03:56 -0500

    Initial

---
+++ LinearPlugin.R
@@ -0,0 +1,58 @@
+###################################################
+### code chunk number 1: sva.Rnw:5-6
+###################################################
+options(width=65)
+
+
+###################################################
+### code chunk number 3: input
+###################################################
+library(sva)
+library(bladderbatch)
+#data(bladderdata)
+library(pamr)
+library(limma)
+
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+pheno = read.csv(paste(pfix, parameters["pheno", 2], sep="/"))
+edata = as.matrix(read.csv(paste(pfix, parameters["edata", 2], sep="/")))
+mod = model.matrix(~as.factor(cancer), data=pheno)
+mod0 = model.matrix(~1,data=pheno)
+n.sv = as.integer(parameters["numsv", 2])#num.sv(edata,mod,method="leek")
+
+	###################################################
+### code chunk number 9: input
+###################################################
+svobj = sva(edata,mod,mod0,n.sv=n.sv)
+
+###################################################
+### code chunk number 6: input
+###################################################
+mod = model.matrix(~as.factor(cancer), data=pheno)
+
+
+###################################################
+### code chunk number 11: input
+###################################################
+modSv = cbind(mod,svobj$sv)
+
+
+fit = lmFit(edata, modSv)
+saveRDS(fit, outputfile)
+
+}
